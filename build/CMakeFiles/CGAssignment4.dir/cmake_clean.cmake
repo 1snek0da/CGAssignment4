a9f6cadaed9120e55422a2677bf6056253c932b8
1snek0da <chenclin3@mail2.sysu.edu.cn>
2025-01-09 14:39:10 +0800

    6.2

---
+++ build/CMakeFiles/CGAssignment4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CGAssignment4"
+  "CGAssignment4.pdb"
+  "CMakeFiles/CGAssignment4.dir/src/WindowsApp.cpp.o"
+  "CMakeFiles/CGAssignment4.dir/src/WindowsApp.cpp.o.d"
+  "CMakeFiles/CGAssignment4.dir/src/main.cpp.o"
+  "CMakeFiles/CGAssignment4.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGAssignment4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
